1b079a939837a66bca8e011a91f04156f522e743
Rafael Pereira Girolineto <rafapg.85@gmail.com>
2017-03-28 19:10:07 -0300

    Added line number functions. Deprecated df, af, cf functions

--- bin/rgit.sh
+++ bin/rgit.sh
@@ -1,4 +1,5 @@
 COMMAND=$1
+LINE_NUMBER=$2
 
 FILE_LIB="NULL"
 RAW_CMD="NULL"
@@ -6,15 +7,34 @@ AWK_VAR_CMD=""
 
 case $COMMAND in
  "df" | "diffirst")
-	FILE_LIB="/Users/Rafael/dev/tools/rgit/commands/cmd_first_staged_line.awk"
+	FILE_LIB="/Users/Rafael/dev/tools/rgit/commands/cmd_line_staged.awk"
+	LINE=1
 	AWK_VAR_CMD="git diff -b"
 	;;
+ "d")
+	FILE_LIB="/Users/Rafael/dev/tools/rgit/commands/cmd_line_staged.awk"
+	LINE=$LINE_NUMBER
+	AWK_VAR_CMD="git diff -b"
+	echo "> $LINE"
+	;;
  "af" | "addfirst")
-	FILE_LIB="/Users/Rafael/dev/tools/rgit/commands/cmd_first_staged_line.awk"
+	FILE_LIB="/Users/Rafael/dev/tools/rgit/commands/cmd_line_staged.awk"
+	LINE=1
+	AWK_VAR_CMD="git add"
+	;;
+ "a")
+	FILE_LIB="/Users/Rafael/dev/tools/rgit/commands/cmd_line_staged.awk"
+	LINE=$LINE_NUMBER
 	AWK_VAR_CMD="git add"
 	;;
  "cf" | "checkoutfirst")
-	FILE_LIB="/Users/Rafael/dev/tools/rgit/commands/cmd_first_staged_line.awk"
+	FILE_LIB="/Users/Rafael/dev/tools/rgit/commands/cmd_line_staged.awk"
+	LINE=1
+	AWK_VAR_CMD="git checkout"
+	;;
+ "c")
+	FILE_LIB="/Users/Rafael/dev/tools/rgit/commands/cmd_line_staged.awk"
+	LINE=$LINE_NUMBER
 	AWK_VAR_CMD="git checkout"
 	;;
  "s" | "status")
@@ -35,9 +55,12 @@ case $COMMAND in
 	echo "(R)afael (GI)rolineto (T)ool"
 	echo ""
 	echo "Commands avaliable:"
-	echo "  df  | diffirst         Runs a git diff with the first modified file of the status"
-	echo "  af  | addfirst         Adds file first file to the stagged files"
-	echo "  cf  | checkoutfirst    Returns the first file to the last commited status"
+	echo " * df | diffirst        [Deprecated] Runs a git diff with the first modified file of the status"
+	echo "  d [POSITION]           Runs a git diff with the [POSITION] modified file of the status"
+	echo " * af | addfirst        [Deprecated] Adds first file to the stagged files"
+	echo "  a [POSITION]           Adds the [POSITION] modified file of the status"
+	echo " * cf | checkoutfirst   [Deprecated] Revert changes of the first file to the last commited status"
+	echo "  d [POSITION]           Revert changes of the [POSITION] modified file of the status"
 	echo "  s   | status           Shows the current git local status"
 	echo "  s-p | status-porcelain Same than status, but with option --porcelain"
 	echo "  cm  | commit           Commits local changes - Requires the commit message"
@@ -50,7 +73,7 @@ esac
 
 if [ "$FILE_LIB" != "NULL" ];
   then
-	git status --porcelain | awk -v cmd="$AWK_VAR_CMD" -f $FILE_LIB
+	git status --porcelain | awk -v cmd="$AWK_VAR_CMD" -v target_line="$LINE" -f $FILE_LIB
 fi
 
 if [ "$RAW_CMD" != "NULL" ];
---
+++ commands/cmd_line_staged.awk
@@ -0,0 +1,35 @@
+BEGIN{current_line=1}
+{ 
+	staged_status=substr($0,1,1);
+	not_staged_status=substr($0,2,1);
+
+	renamed = "R";
+	modified = "M";
+	added = "A";
+	deleted = "D";
+	unknown = "?";
+	null_status = " ";
+
+	if (not_staged_status != null_status)
+	{
+		if (current_line == target_line)
+		{
+			if (staged_status == renamed)
+			{
+				targetFile = $4;
+			}
+			else
+			{
+				targetFile = $2;
+			}
+			print ("[ " staged_status " | " not_staged_status " ] EXEC: " cmd " " targetFile) "\n\n";
+			print "";
+			system (cmd " " targetFile);
+			exit;
+		}
+		else
+		{
+			current_line++;
+		}
+	}
+}
